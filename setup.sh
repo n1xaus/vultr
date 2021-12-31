b56999cdf7f25db8e0eab31340f03b215b35bd80
n1xaus <91096197+n1xaus@users.noreply.github.com>
2021-12-31 13:15:48 +1100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Installing Stuff"
+apt install apache2
+apt install net-tools
+
+echo "Change SSH port to something else"
+echo "WARNING: Choose an unused port!"
+echo "What do you want the port to be? Just type numbers."
+read newport
+echo "Port $newport" >> /etc/ssh/sshd_config
+
+echo "Changing Firewall"
+ufw deny 22
+ufw allow $newport
+ufw allow in "Apache"
+ufw status
+
+echo "Restarting SSH"
+systemctl restart sshd
+netstat -tulpn | grep ssh
+
+echo "Cleaning index.html"
+cd /var/www/html
+echo "<!DOCTYPE html><html><head><title>...</title></head><body><h1>Good Morning!</h1></body></html>" > index.html
+
+echo "We're done here... Visit the server IP and it should say Good Morning"
